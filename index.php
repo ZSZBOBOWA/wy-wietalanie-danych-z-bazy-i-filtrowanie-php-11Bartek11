b734c28ce86183262a55a6e72f0c4d8eaea957cd
Bartlomiej Szczepanek <bartlomiej.szczepanek@zsz.bobowa.pl>
2024-10-24 08:50:36 +0200

    1. Tworzenie Strony PHP do Wyświetlania Danych (Podejście Proceduralne) v3(działająca)

--- index.php
+++ index.php
@@ -13,15 +13,14 @@
         die("Błąd połączenia: ".mysqli_connect_error());
     }
         $sql = "SELECT * FROM uczniowie";
-    
+        $result = mysqli_query($conn,$sql);
         if(mysqli_num_rows($result)>0){
             echo"<table border='1'><tr><th>Imię</th>
-            <th>Nazwisko</th><th>Wiek</th></tr>"
+            <th>Nazwisko</th><th>Wiek</th></tr>";
             while($row = mysqli_fetch_assoc($result)){
-                echo "<tr><td>".$row['imie']"</td>
-                <td>".$row['nazwisko']"</td>
-                <td>".$row['wiek']"</td>
-                </tr>";
+                echo "<tr><td>".$row["imie"]."</td>
+                <td>".$row["nazwisko"]."</td>
+                <td>".$row["wiek"]."</td></tr>";
             }
             echo"</table";
         }else{
